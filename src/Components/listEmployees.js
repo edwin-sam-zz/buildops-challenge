f93e684f60831c8f00a264e0b3d5eac119114857
Edwin Sam <edwin@edwins-mbp.lan>
2020-11-08 21:51:33 -0800

    employee info rendering

--- src/Components/listEmployees.js
+++ src/Components/listEmployees.js
@@ -7,10 +7,24 @@ import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const listEmployees = () => {
-    return (
+class listEmployees extends Component {
+
+     constructor(props) {
+        super(props)
+        this.state = {
+            employees: [],
+            message: null
+        }
+        // this.deleteEmployee = this.deleteEmployee.bind(this);
+        // this.editEmployee = this.editEmployee.bind(this);
+        // this.addEmployee = this.addEmployee.bind(this);
+        // this.reloadEmployeeList = this.reloadEmployeeList.bind(this);
+    }
+
+    render(){
+        return (
             <div>
-                <Button variant="contained" color="primary" onClick={() => this.addUser()}>
+                <Button variant="contained" color="primary" onClick={() => this.addEmployee()}>
                     Add Employee
                 </Button>
 
@@ -23,10 +37,29 @@ const listEmployees = () => {
                             <TableCell>Skills</TableCell>
                         </TableRow>
                     </TableHead>
+
+                    <TableBody>
+                        {this.state.employees.map(row => (
+                            <TableRow key={row.id}>
+                                <TableCell component="th" scope="row">
+                                    {row.id}
+                                </TableCell>
+                                <TableCell align>{row.id}</TableCell>
+                                <TableCell align>{row.firstName}</TableCell>
+                                <TableCell align>{row.lastName}</TableCell>
+                                <TableCell align>{row.skills}</TableCell>
+                                <TableCell align onClick={() => this.editUser(row.id)}>Edit</TableCell>
+                                <TableCell align onClick={() => this.deleteUser(row.id)}>Delete</TableCell>
+
+                            </TableRow>
+                        ))}
+                    </TableBody>
                 </Table>
 
             </div>
-    )
+        )
+    }
+    
 }
 
 export default listEmployees;
