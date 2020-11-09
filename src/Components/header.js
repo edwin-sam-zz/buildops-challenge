feab05add45cedd47642ae0ac6c7cdf95fd31976
Edwin Sam <edwin@edwins-mbp.lan>
2020-11-08 18:53:15 -0800

    material ui addition

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,14 +1,50 @@
-import React from 'react'
-import '../index.css'
-import logo from '../logo.png'
+// import React from 'react'
+// import '../index.css'
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+// import logo from '../logo.png'
 
+// const Header = () => {
+//     return (
+//         <div className="header">
+//             <header>
+//                 <IconButton edge="start" color="inherit" aria-label="Menu">
+//                         Menu
+//                     </IconButton>
+//                 <img className="header-img" src={logo} />
+//         </header>
+//     </div>
+//     )
+// }
+
+// export default Header;
+
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const style = {
+    flexGrow: 1
+}
 const Header = () => {
     return (
-        <div className="header">
-            <header>
-                <img className="header-img" src={logo} />
-        </header>
-    </div>
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="Menu">
+                        Menu
+                    </IconButton>
+                    <Typography variant="h6">
+                        React User Application
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
     )
 }
 
--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './index.css'
 import Button from '@material-ui/core/Button';
+import 'fontsource-roboto';
 
 const App = () => {
     return (
         <div>
+            <h1 className="details">Employee Details</h1>
             <Button variant="contained" color="default" className="addButton">Add Employee</Button>
         </div>
     )
